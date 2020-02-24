ec0e759e60db134fafcb4abce6994cea08dbe218
Stoyan Kalinov <bloodyrax@gmail.com>
2020-02-24 14:34:15 +0200

    completing assignment lecture - 136. JPA Enumerations

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -31,6 +31,9 @@ public class Recipe {
     @Lob
     private Byte[] image;
 
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
+
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
 
